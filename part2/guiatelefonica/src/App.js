29ce6291f929fbd9f25f03f506694cdd00d27f69
maironcuello <mjcm2021@gmail.com>
2023-01-02 15:06:38 -0500

    refactory Component App and file config

--- part2/guiatelefonica/src/App.js
+++ part2/guiatelefonica/src/App.js
@@ -1,19 +1,15 @@
 import React, { useState, useEffect } from "react";
-
-/**
- * Our Styles
- */
 import "./App.css";
-/**
- * In this section import the function and Methods from helpers
- */
 import { isSomePerson, findContact } from "./helpers/config";
+
 /**
  * Import our Components
  */
 import { FilterContact } from "./components/FilterContact";
 import { AddnewContact } from "./components/AddnewContact";
 import { ShowContact } from "./components/ShowContact";
+import { Notification } from "./components/Notification";
+
 import { getAllContact, createContact, updateContact } from "./helpers/phone";
 
 export const App = () => {
@@ -21,37 +17,48 @@ export const App = () => {
    * Uses States
    */
   const [persons, setPersons] = useState([]);
+  const [personsFilter, setPersonsFilter] = useState("");
   const [newName, setNewName] = useState("");
   const [phoneNumber, setPhoneNumber] = useState("");
   const [findName, setFindName] = useState("");
+  const [messagError, setMessagError] = useState(null);
 
   useEffect(() => {
     getAllContact(setPersons);
   }, []);
-  
+
+  const filterPerson = (event) =>{
+    setFindName(event.target.value);
+    const filteredContact = persons.filter((person) => person.name.toLowerCase().includes(findName.toLowerCase()));
+    setPersonsFilter(filteredContact);
+  }
+ 
   const addContact = (event) => {
     event.preventDefault();
     if (isSomePerson(newName, persons)) {
       const contact = findContact(newName, persons);
       const id = contact[0].id;
       const contactUpdate = { name: newName, phone: phoneNumber };
-      if (window.confirm(`${newName} is already added to phonebook, do you want to replace it?`)){
-        updateContact(id, contactUpdate)
-        alert('Contact already to update successfully')
+      if (
+        window.confirm(
+          `${newName} is already added to phonebook, do you want to replace it?`
+        )
+      ) {
+        updateContact(id, contactUpdate);
+        alert("Contact already to update successfully");
       }
     } else {
-        const contact = { name: newName, phone: phoneNumber };
-        createContact(contact);
-        setPersons([...persons, contact]);
-        setNewName("");
-        setPhoneNumber("");
-        alert('Contact added to phonebook successfully')
+      const contact = { name: newName, phone: phoneNumber };
+      createContact(contact);
+      setPersons([...persons, contact]);
+      setNewName("");
+      setPhoneNumber("");
     }
   };
 
   const handleNameChange = (event) => setNewName(event.target.value);
   const handlePhoneChange = (event) => setPhoneNumber(event.target.value);
-  const handleFindNameChange = (event) => setFindName(event.target.value);
+  // const handleFindNameChange = (event) => setFindName(event.target.value);
 
   return (
     /**
@@ -62,10 +69,11 @@ export const App = () => {
         <FilterContact
           title="Filter shown with"
           findname={findName}
-          onChange={handleFindNameChange}
-          persons={persons}
+          filterPerson={filterPerson}
+          // persons={persons}
         />
         <hr />
+        <Notification />
         <AddnewContact
           title="Phonebook"
           newname={newName}
