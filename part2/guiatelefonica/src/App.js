3ee0f09fa7cf35c40f8236102810c8c026287a87
maironcuello <mjcm2021@gmail.com>
2023-01-03 16:36:23 -0500

    Refactor Component App

--- part2/guiatelefonica/src/App.js
+++ part2/guiatelefonica/src/App.js
@@ -10,32 +10,32 @@ import { AddnewContact } from "./components/AddnewContact";
 import { ShowContact } from "./components/ShowContact";
 import { Notification } from "./components/Notification";
 
-import { getAllContact, createContact, updateContact } from "./helpers/phone";
+/**
+ * Import our methods for interacting with the rest server
+ */
+import {getAllContact, createContact, updateContact, deleteContact,} from "./helpers/phone";
 
 export const App = () => {
-  /**
-   * Uses States
-   */
+
   const [persons, setPersons] = useState([]);
   const [personsFilter, setPersonsFilter] = useState([]);
   const [newName, setNewName] = useState("");
   const [phoneNumber, setPhoneNumber] = useState("");
   const [findName, setFindName] = useState("");
-  const [messagError, setMessagError] = useState(null);
+  const [message, setMessage] = useState(null);
 
   useEffect(() => {
-    getAllContact(setPersons).then(data => {
-    });
+    getAllContact(setPersons).then((data) => {});
   }, []);
 
   const filterPerson = (event) => {
     event.preventDefault();
-    let eventPerson = event.target.value;
-    console.log(eventPerson);
+    const eventPerson = event.target.value;
     setFindName(eventPerson);
-    // console.log(findName);
-    const filteredContact = persons.filter((person) => person.name.toLowerCase().includes(eventPerson.toLowerCase()));
-    return (eventPerson !== "") ? setPersonsFilter(filteredContact) : setPersonsFilter([]);
+    const filteredContact = persons.filter((person) =>
+      person.name.toLowerCase().includes(eventPerson.toLowerCase())
+    );
+    return eventPerson !== "" ? setPersonsFilter(filteredContact) : setPersonsFilter([]);
   };
 
   const addNewContactOrUpdate = (event) => {
@@ -44,44 +44,50 @@ export const App = () => {
       const contact = findContact(newName, persons);
       const id = contact[0].id;
       const contactUpdate = { name: newName, phone: phoneNumber };
-      if (
-        window.confirm(
-          `${newName} is already added to phonebook, do you want to replace it?`
-        )
-      ) {
-        updateContact(id, contactUpdate).then(data => {
-          setPhoneNumber("");
-          getAllContact(setPersons)
-        })
+      if (window.confirm(`${newName} is already added to phonebook, do you want to replace it?`)) {
+        updateContact(id, contactUpdate)
+        .then((data) => {
+          getAllContact(setPersons);
+          setNewName("");
+          setPhoneNumber("");    
+        });
       }
     } else {
       const contact = { name: newName, phone: phoneNumber };
-      createContact(contact);
+      createContact(contact).then((data) => {
+        setMessage(`New contact ${data.name} added successfully`);
+        setTimeout(() => {
+          setMessage(null);
+        }, 5000);
+      });
       setPersons([...persons, contact]);
       setNewName("");
       setPhoneNumber("");
     }
   };
 
-  const dataContact = {
+  const parametersAddnewContact = {
     newName,
     phoneNumber,
     addNewContactOrUpdate,
     setNewName,
     setPhoneNumber,
   };
-
+  const parametersShowContact = {
+    personsFilter,
+    setPersonsFilter,
+    deleteContact,
+    getAllContact,
+    setPersons,
+  };
   return (
-    /**
-     * In this section, shown the components
-     */
-    <div className="container">
+
+  <div className="container">
       <div className="components">
-        <Notification />
+        <Notification messege={message} />
         <FilterContact findname={findName} filterPerson={filterPerson} />
-        <AddnewContact dataContact={dataContact}/>
-        <hr />
-        <ShowContact persons={persons} personsFilter={personsFilter} />
+        <AddnewContact parametersAddnewContact={parametersAddnewContact} /><hr/>
+        <ShowContact parametersShowContact={parametersShowContact} />
       </div>
     </div>
   );
