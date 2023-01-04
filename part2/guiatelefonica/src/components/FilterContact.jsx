f2afba9fc9403107c9d86faf801fa7f135ad9f7f
maironcuello <mjcm2021@gmail.com>
2023-01-04 13:43:59 -0500

    Refactor FilterContact

--- part2/guiatelefonica/src/components/FilterContact.jsx
+++ part2/guiatelefonica/src/components/FilterContact.jsx
@@ -1,13 +1,18 @@
+import React, { Fragment } from "react";
 /**
- * 
- * @param {findname, filterPerson}  
+ * @param {findname, filterPerson}
  * @returns Render input to filter person name
  */
 export const FilterContact = ({ findname, filterPerson }) => {
   return (
-    <div>
+    <Fragment>
       <h2>{"Filter shown with"}</h2>
-      <input value={findname} required onChange={filterPerson} placeholder="Contact" />
-    </div>
+      <input
+        value={findname}
+        required
+        onChange={filterPerson}
+        placeholder="Contact"
+      />
+    </Fragment>
   );
 };
