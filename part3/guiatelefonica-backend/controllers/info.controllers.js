d260e3f43e831e84600cf4b2da328a60bf9d8e58
maironcuello <mjcm2021@gmail.com>
2023-01-06 17:37:01 -0500

    Refactor controllers route info

--- part3/guiatelefonica-backend/controllers/info.controllers.js
+++ part3/guiatelefonica-backend/controllers/info.controllers.js
@@ -1,11 +1,12 @@
 const { request, response } = require("express");
-const db = require("../database/db.json");
-
+const {getAllContacts} = require('./../helpers/database/dbConfigurations')
 
 const getInfo = (req = request, res = response) => {
+  const contacts = getAllContacts();
+    
   res.status(200).send(
     `
-    <h3>Phonebook has info for ${db.length} people</h3>
+    <h3>Phonebook has info for ${contacts.length} people</h3>
     <h3>${Date()}</h3>
     `
   );
