5df3c7ac0c8e3bb216b9e57e200d97f4cb48f8e2
maironcuello <mjcm2021@gmail.com>
2023-01-06 17:41:00 -0500

    Refactor route PUT

--- part3/guiatelefonica-backend/routes/persons.routes.js
+++ part3/guiatelefonica-backend/routes/persons.routes.js
@@ -1,5 +1,12 @@
 const { Router } = require("express");
-const { getAllPersons, getPerson, deletePerson } = require("../controllers/persons.controllers");
+const morgan = require("morgan");
+const {
+  getAllPersons,
+  getPerson,
+  createPerson,
+  deletePerson,
+  updatePerson,
+} = require("../controllers/persons.controllers");
 
 /**
  * Router
@@ -7,10 +14,17 @@ const { getAllPersons, getPerson, deletePerson } = require("../controllers/perso
 const router = Router();
 
 /**
+ * Creating morgan token from JSON object
+ */
+morgan.token("body", (request) => request.method === 'POST'? JSON.stringify(request.body): '');
+
+/**
  * Routes for the server
  */
 router.get("/", getAllPersons);
 router.get("/:id", getPerson);
+router.post("/", createPerson);
+router.put("/:id", updatePerson);
 router.delete("/:id", deletePerson);
 
 module.exports = router;
