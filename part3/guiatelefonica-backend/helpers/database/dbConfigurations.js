f7b5a95bf2ba845063f8efd2ee112a26cd220ecb
maironcuello <mjcm2021@gmail.com>
2023-01-06 17:35:27 -0500

    Refactor method for consult database

--- part3/guiatelefonica-backend/helpers/database/dbConfigurations.js
+++ part3/guiatelefonica-backend/helpers/database/dbConfigurations.js
@@ -1,12 +1,50 @@
 const db = require("../../database/db.json");
 
+let dbPhonebook = [] = db;
 
+/**
+ * @param {*} id
+ * @returns Find contact with given id
+ */
+const getContactById = (id) => dbPhonebook.find((ele) => ele.id === id);
 
-const getContactById = (id) => db.find((ele)=> ele.id === id);    
-const getAllContacts = () => db;    
+/**
+ * @returns All contacts from database
+ */
+const getAllContacts = () => dbPhonebook;
 
+/**
+ * @returns Create a new id for the new contact
+ */
+const createPersonId = () => (maxId = dbPhonebook.length > 0 ? Math.max(...dbPhonebook.map((n) => n.id + 1)) : 1);
+
+/**
+ * @param {*} name
+ * @returns Check if contact with given name exists in database
+ */
+const getContactByName = (name) => (foundContactName = dbPhonebook.find((ele) => ele.name === name));
+
+/**
+ * @param {*} person
+ * @returns Create a new contact in database
+ */
+const createContact = (person) => dbPhonebook.push(person);
+
+/**
+ * 
+ * @param {*} id 
+ * Delete contact with given id
+ */
+const deleteContact = (id) => {
+  const index = dbPhonebook.findIndex((ele) => ele.id === id);
+  dbPhonebook.splice(index, 1);
+};
 
 module.exports = {
-    getContactById,
-    getAllContacts
-}
+  getContactById,
+  getContactByName,
+  getAllContacts,
+  createPersonId,
+  createContact,
+  deleteContact,
+};
