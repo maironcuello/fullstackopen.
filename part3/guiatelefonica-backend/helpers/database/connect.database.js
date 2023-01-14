bd4eb5dc2c2fb2a76bf19d10fcb7700fd7b1edf8
maironcuello <mjcm2021@gmail.com>
2023-01-14 14:56:57 -0500

    Create Middleware method logger 01|14|2023 02:25:PM

--- part3/guiatelefonica-backend/helpers/database/connect.database.js
+++ part3/guiatelefonica-backend/helpers/database/connect.database.js
@@ -1,20 +1,20 @@
 const mongoose = require('mongoose');
 const { User } = require('../../models/phonebook.model');
 require('dotenv').config();
+const logger = require('.././utils/logger')
 
-
+mongoose.set("strictQuery", false);
 const dbConection = async () => {
-    try {
-        await mongoose.connect(process.env.MONGO_CNN, {
-            useNewUrlParser: true,
-            useUnifiedTopology: true
-        });
 
-        console.log(`Database connection is established. status: ${process.env.NAME} is online`);
-
-    } catch (error) {
-        throw new Error(`Database connection not established. status: ${process.env.NAME} is offline`);
-    }
+    await mongoose.connect(process.env.MONGO_CNN, {
+        useNewUrlParser: true,
+        useUnifiedTopology: true
+    })
+        .then(() => {
+            logger.info(`Database connection ${process.env.NAME} is established. status : Online`);
+        }).catch((err) => {
+            throw new Error(`Database connection ${process.env.NAME} not established. status:  Offline`);
+        })
 }
 
 module.exports = {
---
+++ part3/guiatelefonica-backend/helpers/utils/config.port.server.js
@@ -0,0 +1,6 @@
+/**
+ * Employ to the production environment
+ */
+process.env.PORT = process.env.PORT || 8000;
+MONGO_CNN = process.env.MONGO_CNN;
+
