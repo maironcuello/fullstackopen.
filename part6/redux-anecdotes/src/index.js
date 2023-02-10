b08fb85ba9ea73950c3a9b7528053c00d465c2bf
maironcuello <mjcm2021@gmail.com>
2023-02-10 17:30:40 -0500

    Exercise 6.15, 6.16, 6.17, 6.18,6.19, 6.20 and 6.21

--- part6/redux-anecdotes/src/index.js
+++ part6/redux-anecdotes/src/index.js
@@ -4,13 +4,6 @@ import { Provider } from 'react-redux'
 import { App } from './App'
 import { store } from './store'
 
-// ReactDOM.render(
-//   <Provider store={store}>
-//     <App />
-//   </Provider>,
-//   document.getElementById('root')
-// )
-
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
